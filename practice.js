c5d629e8c6213c1eaee2950397b7820ceffa9989
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-09-12 08:10:22 +0800

    final commit

---
+++ practice.js
@@ -0,0 +1,11 @@
+let name = 'Aaron Raymund R. Paguirigan';
+console.log(name);
+
+let whatDoYouWannaBecomeInYourLife = 'Programmer';
+console.log('What Do You Wanna Become In Your Life:', whatDoYouWannaBecomeInYourLife);
+
+let gender = 'Male';
+console.log(gender);
+
+let twitterHandle = 'Arzz_08';
+console.log('twitter handle:',twitterHandle);
